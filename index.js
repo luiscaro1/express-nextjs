fddb93ff538774bce38299c8e3f64a6bfa9f7442
Luis Caro <luis.caro1@upr.edu>
2020-09-03 19:06:11 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const Next = require("next");
+
+const PORT = process.env.PORT || 5000;
+
+const dev = process.env.NODE_ENV !== "production";
+
+//might need to change this
+app = Next({ dev });
+
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.use(bodyparser.urlencoded({ extended: false }));
+    server.use(bodyparser.json({ limit: "50mb" }));
+
+    server.use("/api/hello", (req, res) => {
+      res.send({ msg: "Hello" });
+    });
+
+    server.listen(PORT, (err) => {
+      if (err) throw err;
+
+      console.log(`App is running in port ${PORT}`);
+    });
+  })
+  .catch(() => {
+    process.exit(1);
+  });
